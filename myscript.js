ca1f6ddb5d4ad70d9953428af608bf288df316ed
andrew <andrewh23@protonmail.com>
2022-05-26 18:52:31 -0700

    create prompt to declare user win or lose and adjust script to be case insensitive

--- myscript.js
+++ myscript.js
@@ -10,12 +10,30 @@ var choices = ["rock", "paper", "scissors"]
 // console.log(computerPlay(choices)); //
 
 function playRound (playerSelection, computerSelection) { 
-    var choose = prompt("rock paper or scissors");
-    if (choose == playerSelection || "paper" || "scissors") {
-        return computerSelection}
-}
+    var choose = prompt("rock paper or scissors").toLowerCase();
+    if (choose == "rock" && computerSelection == "paper") {alert( "You lose! Paper beats rock!");}
+
+    else if (choose == "rock" && computerSelection == "scissors") {alert( "You win! Rock beats scissors");}
+    
+    else if (choose == "rock" && computerSelection == "rock") {alert( "Draw!");}
+
+    else if (choose == "paper" && computerSelection == "scissors") {alert( "You Lose! Scissors beats paper");}
+
+    else if (choose == "paper" && computerSelection == "rock") {alert( "You win! Paper beats rock!");}
+
+    else if (choose == "paper" && computerSelection == "paper") {alert( "Draw!");}
+
+    else if (choose == "scissors" && computerSelection == "rock") {alert( "You lose! Rock beats scissors");}
+
+    else if (choose == "scissors" && computerSelection == "paper") {alert( "You win! Scissors beat paper!");}
+
+    else if (choose == "scissors" && computerSelection == "scissors") {alert( "Draw!");}
+
+    else (choose == playerSelection || "paper" || "scissors") 
+        return computerSelection
 
     
+}
 
 
 
