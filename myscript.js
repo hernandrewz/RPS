6b3c57729b82c96dfeab9cdcbfbbb706c3444afe
andrew <andrewh23@protonmail.com>
2022-05-26 17:11:53 -0700

    create playround function. make function trigger computerplay when one of 3 terms are input

--- myscript.js
+++ myscript.js
@@ -7,4 +7,18 @@ function computerPlay(RPS) {
 
 var choices = ["rock", "paper", "scissors"]
 
-console.log(computerPlay(choices));
+// console.log(computerPlay(choices)); //
+
+function playRound (playerSelection, computerSelection) { 
+    var choose = prompt("rock paper or scissors");
+    if (choose == playerSelection || "paper" || "scissors") {
+        return computerSelection}
+}
+
+    
+
+
+
+const playerSelection = "rock" ;
+const computerSelection = computerPlay();
+console.log(playRound(playerSelection,computerSelection));
