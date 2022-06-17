b4d1498dfafe6fd0903e71c059b3b29c4d52570e
andrew <andrewh23@protonmail.com>
2022-06-17 11:48:43 -0700

    branch copy of RPS game from main to temp branch

--- myscript.js
+++ myscript.js
@@ -16,25 +16,25 @@ var computerScore = 0;
 
 function playRound (playerSelection, computerSelection) { 
     var choose = prompt("rock paper or scissors").toLowerCase();
-    var computerSelection = computerPlay();
+  
 
-    if (choose == "rock" && computerSelection == "paper") {alert( "You lose! Paper beats rock!"); computerScore++;}
+    if (choose == "rock" && computerPlay() == "paper") {console.log( "You lose! Paper beats rock!"); computerScore++;} // where "computerPlay" sits, the line used to have "computerSelection" , but the latter string registers as a paramter instead of function which doesn't randomize computers choice when game is played //
 
-    else if (choose == "rock" && computerSelection == "scissors") {alert( "You win! Rock beats scissors"); playerScore++;}
+    else if (choose == "rock" && computerPlay() == "scissors") {console.log("You win! Rock beats scissors"); playerScore++;}
     
-    else if (choose == "rock" && computerSelection == "rock") {alert( "Draw!");}
+    else if (choose == "rock" && computerPlay()== "rock") {console.log( "Draw!");}
 
-    else if (choose == "paper" && computerSelection == "scissors") {alert( "You Lose! Scissors beats paper"); computerScore++;}
+    else if (choose == "paper" && computerPlay() == "scissors") {console.log( "You Lose! Scissors beats paper"); computerScore++;}
 
-    else if (choose == "paper" && computerSelection == "rock") {alert( "You win! Paper beats rock!"); playerScore++;}
+    else if (choose == "paper" && computerPlay() == "rock") {console.log( "You win! Paper beats rock!"); playerScore++;}
 
-    else if (choose == "paper" && computerSelection == "paper") {alert( "Draw!");}
+    else if (choose == "paper" && computerPlay() == "paper") {console.log( "Draw!");}
 
-    else if (choose == "scissors" && computerSelection == "rock") {alert( "You lose! Rock beats scissors"); computerScore++;}
+    else if (choose == "scissors" && computerPlay() == "rock") {console.log( "You lose! Rock beats scissors"); computerScore++;}
 
-    else if (choose == "scissors" && computerSelection == "paper") {alert( "You win! Scissors beat paper!"); playerScore++;}
-
-    else if (choose == "scissors" && computerSelection == "scissors") {alert( "Draw!");}
+    else if (choose == "scissors" && computerPlay() == "paper") {console.log( "You win! Scissors beat paper!"); playerScore++;}
+ 
+    else if (choose == "scissors" && computerPlay() == "scissors") {console.log( "Draw!");}
 
     else ((choose == playerSelection || "paper" || "scissors") && computerSelection); console.log("Computer" + " " + computerScore);
 console.log("Player" + " " + playerScore);
@@ -44,11 +44,11 @@ console.log("Player" + " " + playerScore);
 function game () {
 for (let i = 0; i < 6; i++) {
     console.log("ROUND" + " " + i );
-    if (i == 5 && playerScore > computerScore) {console.log("YOU WIN"); break;}
+    if (i == 5 && playerScore > computerScore) {console.log("YOU WIN THE FIVE ROUND MATCH"); break;}
 
-    else if (i == 5 && computerScore > playerScore) {console.log("YOU LOSE"); break;}
+    else if (i == 5 && computerScore > playerScore) {console.log("YOU LOSE THE FIVE ROUND MATCH"); break;}
 
-    else if (i == 5 && computerScore == playerScore) {console.log("TIE"); break;}
+    else if (i == 5 && computerScore == playerScore) {console.log("YOU TIE THE 5 ROUND MATCH"); break;}
 
     
 
