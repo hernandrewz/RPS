c39e85230ae2aac3c07aeda4dbd33389cdac28b4
andrew <andrewh23@protonmail.com>
2022-06-20 08:58:45 -0700

    add buttons to ui, add nonfunction score table, modify function to populate first paramater with button choice, make game work with button choice instead of type prompt input

--- myscript.js
+++ myscript.js
@@ -11,57 +11,71 @@ var playerScore = 0;
 
 var computerScore = 0;
 
-document.createElement('input');
-Element.setAttribute('text');
-const input = document.createElement("inpout");
-input.setAttribute("type", "text");
+
 // console.log(computerPlay(choices)); //
 
-function playRound (playerSelection, computerSelection) { 
-    var choose = prompt("rock paper or scissors").toLowerCase();
+const choice1 = document.getElementById("chosenRock");
+
+const choice2 = document.getElementById("chosenPaper");
+
+const choice3 = document.getElementById("chosenScissors");
+
+const playerSelection1 ="rock";
+const playerSelection2 = "paper";
+const playerSelection3 = "scissors";
+const playerSelection = choice1 || choice2 || choice3; 
+choice1.addEventListener("click",function(){playRound("rock", computerPlay);});
+    
+choice2.addEventListener("click", function(){playRound("paper", computerPlay);});
+
+choice3.addEventListener("click", function(){playRound("scissors", computerPlay);});
+
+
+function playRound (playerSelection, computerSelection) {  // howto get first paramter to register as button click instead of defaulting to playerSelection1?//
+    var choose = playerSelection; 
     var computerSelection = computerPlay();
 
-    if (choose == "rock" && computerSelection == "paper") {alert( "You lose! Paper beats rock!"); computerScore++;}
+    if (choose == "rock" && computerSelection == "paper") {console.log( "You lose! Paper beats rock!"); computerScore++;}
 
-    else if (choose == "rock" && computerSelection == "scissors") {alert( "You win! Rock beats scissors"); playerScore++;}
+    else if (choose == "rock" && computerSelection == "scissors") {console.log( "You win! Rock beats scissors"); playerScore++;}
     
-    else if (choose == "rock" && computerSelection == "rock") {alert( "Draw!");}
+    else if (choose == "rock" && computerSelection == "rock") {console.log( "Draw!");}
 
-    else if (choose == "paper" && computerSelection == "scissors") {alert( "You Lose! Scissors beats paper"); computerScore++;}
+    else if (choose == "paper" && computerSelection == "scissors") {console.log( "You Lose! Scissors beats paper"); computerScore++;}
 
-    else if (choose == "paper" && computerSelection == "rock") {alert( "You win! Paper beats rock!"); playerScore++;}
+    else if (choose == "paper" && computerSelection == "rock") {console.log( "You win! Paper beats rock!"); playerScore++;}
 
-    else if (choose == "paper" && computerSelection == "paper") {alert( "Draw!");}
+    else if (choose == "paper" && computerSelection == "paper") {console.log( "Draw!");}
 
-    else if (choose == "scissors" && computerSelection == "rock") {alert( "You lose! Rock beats scissors"); computerScore++;}
+    else if (choose == "scissors" && computerSelection == "rock") {console.log( "You lose! Rock beats scissors"); computerScore++;}
 
-    else if (choose == "scissors" && computerSelection == "paper") {alert( "You win! Scissors beat paper!"); playerScore++;}
+    else if (choose == "scissors" && computerSelection == "paper") {console.log( "You win! Scissors beat paper!"); playerScore++;}
 
-    else if (choose == "scissors" && computerSelection == "scissors") {alert( "Draw!");}
+    else if (choose == "scissors" && computerSelection == "scissors") {console.log( "Draw!");}
 
-    else ((choose == playerSelection || "paper" || "scissors") && computerSelection); console.log("Computer" + " " + computerScore);
+console.log("Computer" + " " + computerScore);
 console.log("Player" + " " + playerScore);
-
-    
 }
-function game () {
-for (let i = 0; i < 6; i++) {
-    console.log("ROUND" + " " + i );
-    if (i == 5 && playerScore > computerScore) {console.log("YOU WIN"); break;}
 
-    else if (i == 5 && computerScore > playerScore) {console.log("YOU LOSE"); break;}
 
-    else if (i == 5 && computerScore == playerScore) {console.log("TIE"); break;}
+
+
+
+
+
+
+
+
+
+
+
 
     
 
     
  
-    playRound(playerSelection, computerSelection);
+  
 
-}
-}
 
-const playerSelection = "rock" ;
 const computerSelection = computerPlay();
-console.log(game(playerSelection,computerSelection));
+
